65c7dff5ac7b31d0d8d1604d321f212e0bf9dc13
kenmadhu <41500918+kenmadhu@users.noreply.github.com>
2020-07-27 17:55:38 +0530

    Add files via upload

---
+++ com.AmazonApkAuto/src/test/java/Tests/LoginTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+import Pages.HomePage;
+import TestUtils.ExcelReader;
+
+public class LoginTest extends BaseTest {
+
+	@Test(priority = 0)
+	public void invalidLoginTest_InvalidUserNameInvalidPassword() {
+		HomePage homePage = new HomePage(driver);
+		String username = ExcelReader.getCellData("LoginDetails", "User_email", 2);
+		String password = ExcelReader.getCellData("LoginDetails", "Login_Password", 2);
+		homePage.goToLoginPage().loginToN11(username,password).verifyLoginPassword(("Message")).verifyLoginPassword(("Message"));
+	}
+}
